2e219f6f29af6865e6dd1f30a7fa1a2cfd31ae7c
Kon1Kobryn <konrkob620@student.polsl.pl>
2023-04-18 15:27:08 +0200

    Update CeramicDishManager.java
    h

--- src/main/java/com/example/rest_api_1/manager/CeramicDishManager.java
+++ src/main/java/com/example/rest_api_1/manager/CeramicDishManager.java
@@ -5,6 +5,7 @@ import com.example.rest_api_1.dao.entity.CeramicDish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -33,6 +34,9 @@ public class CeramicDishManager {
 	}
 
 	public void fillDb(){
-		save(new CeramicDish(1L))
+		save(new CeramicDish(1L, "talerz1", LocalDate.of(1980,1,1)));
+		save(new CeramicDish(2L, "talerz2", LocalDate.of(1985,2,15)));
+		save(new CeramicDish(3L, "talerz3", LocalDate.of(1988,3,20)));
+		save(new CeramicDish(4L, "talerz4", LocalDate.of(1995,4,25)));
 	}
 }
